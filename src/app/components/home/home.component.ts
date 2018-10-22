0ec95a5c91e94b695d686aa848a2343036a65845
theCrius <claudio.vallesi@gmail.com>
2018-10-22 15:56:56 +0100

    Basic usage of fontawesome library

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faDiceD20 } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  homeIcon = faDiceD20;
 
   constructor() { }
 
